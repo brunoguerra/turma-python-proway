8256e29b7de1638336011995f29db7fba55105f4
Yuri Hartmann <36309775+yurihartmann@users.noreply.github.com>
2019-11-27 14:23:34 -0300

    Update sorteio.py

--- exercicios/sorteio.py
+++ exercicios/sorteio.py
@@ -1,10 +1,13 @@
 import random
+import time
 
 f=open("alunos.txt", "r")
 
-print('... sorteando')
-if f.mode == 'r':
-  contents =f.read()
-  print(random.sample(contents.split("\n"), 1))
+print('\033[1;34m' + format(" SORTEANDO ", '.<20') + '\033[m', end='\n\n')
+time.sleep(2)
 
-  
+if f.mode == 'r':
+  contents = f.read()
+  print('\033[1;32m' + format((random.sample(contents.split("\n"), 1)[0]).title(), '^30') + '\033[m')
+
+print('\n')  
